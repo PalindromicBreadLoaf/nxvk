df85ca0500c03d9e9793586c325d32d1e316a106
PalindromicBreadLoaf <palindromicbreadloaf@tuta.com>
2026-07-05 19:44:24 -0400

    Create skeleton for WSI interface

--- src/vulkan/wsi/wsi_common.h
+++ src/vulkan/wsi/wsi_common.h
@@ -52,7 +52,11 @@ struct vk_queue;
 
 struct driOptionCache;
 
+#ifdef VK_USE_PLATFORM_VI_NN
+#define VK_ICD_WSI_PLATFORM_MAX (VK_ICD_WSI_PLATFORM_VI + 1)
+#else
 #define VK_ICD_WSI_PLATFORM_MAX (VK_ICD_WSI_PLATFORM_METAL + 1)
+#endif
 
 struct wsi_device {
    /* Allocator for the instance */
